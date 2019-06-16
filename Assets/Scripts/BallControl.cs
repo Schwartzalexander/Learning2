1d914403a2da4f1e7347fc1520caf6963c83a2e9
Alexander Schwartz <development@aschwartz.de>
2019-06-16 21:35:35 +0200

    Adding first playable version. A ball can be moved around a terrain using WASD + space, while the camera can be moved using the mouse (including Mouse wheel). The ball keeps falling through the terrain, if the falling speed is too high.

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+
+    public float xForce = 10.0f;
+    public float yForce = 100.0f;
+    public float zForce = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this is for the X axis' movement (moving left and right)
+        float x = xForce * Input.GetAxis("Horizontal");
+        // this is for the Z axis' movement (moving backwards and forwards)
+        float z = zForce * Input.GetAxis("Vertical");
+
+        // this is for the Y axis' movement (jumping)
+        float y = 0.0f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            y = yForce;
+        }
+
+        GetComponent<Rigidbody>().AddForce(x, y, z);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0f, 0.5f, -2f);
+
+    public float distance = 2;
+
+    // This is for moving the camera left and right
+    public float xLowerLimit = -1f;
+    public float xUpperLimit = 1f;
+    // This is for tilting the camera up and down
+    public float yLowerLimit = 0.3f;
+    public float yUpperLimit = 1f;
+    // This is for the distance of the camera behind the ball
+    public float zLowerLimit = -1f;
+    public float zUpperLimit = 0f;
+
+    // This is for the camera distance
+    public float distanceLowerLimit = 0f;
+    public float distanceUpperLimit = 10;
+    void FixedUpdate()
+    {
+
+        offset /= distance;
+
+        // This is for tilting the camera up and down
+        offset.y += Input.GetAxis("Mouse Y") / 10.0f;
+        // This is for rotating the camera left and right
+        offset.x += Input.GetAxis("Mouse X") / 10.0f;
+
+        distance -= Input.GetAxis("Mouse ScrollWheel");
+
+        distance = setBetween(distance, distanceLowerLimit, distanceUpperLimit);
+        offset.x = setBetween(offset.x, xLowerLimit, xUpperLimit);
+        offset.y = setBetween(offset.y, yLowerLimit, yUpperLimit);
+
+        offset *= distance;
+        offset.z = -distance;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+
+    private float setBetween(float value, float lowerLimit, float upperLimit)
+    {
+        value = Mathf.Max(value, lowerLimit);
+        return Mathf.Min(value, upperLimit);
+    }
+}
