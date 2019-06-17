2a30b197538a97d3e50b8a891d624c3b4fc0b003
Alexander Schwartz <development@aschwartz.de>
2019-06-17 21:41:17 +0200

    Adding loads of items lying around in the world. Great fun!

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(0.0f, 300.0f, 0.0f);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
